c50f0427f867a7d7025e7256b3c8a519168ea391
drishti1515 <87563570+drishti1515@users.noreply.github.com>
2023-12-20 14:38:47 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "drishti@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState
+
+// accountId = 56789     // not allowed in this format
+
+accountEmail = "rawat@gmail.com"
+accountPassword= "2345"
+accountCity = "Banglore"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
