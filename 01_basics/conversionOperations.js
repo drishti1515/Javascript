8a0ae3c02580ab160b9e48a81fd111efc51afad9
drishti1515 <87563570+drishti1515@users.noreply.github.com>
2023-12-20 16:50:57 +0000

    operation conversions

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,37 @@
+let score = 33
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// *********************************Operations*********************************************
+
+let value = 6
+let negValue = -value
+// console.log(negValue);
+// console.log(2**4)
+// console.log(2%2)
+
+let str1= "hello"
+let str2 = "drishti"
+let str3= str1+str2
+// console.log(str3)
+
+// console.log("2" + 1)
+// console.log("2"+"2"+1)
+// console.log(2+2+"1")
+// console.log("1"+2+2)
+
+// console.log(+true);
+
+let gameCounter = 100
+// gameCounter++ =>101
+//++gameCounter  => 101
+// --gameCounter => 99
+// gameCounter-- => 99
+console.log(gameCounter);
