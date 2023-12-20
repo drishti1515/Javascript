367e26f14cae448d8660477f0ebb5cf6d386fe76
drishti1515 <87563570+drishti1515@users.noreply.github.com>
2023-12-20 15:03:10 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // to newer version of all code of js
+
+// alert("hello")   we are using NodeJs not browser 
+
+let name = " Drishti"
+let age= 21
+let address = "jaipur"
+let isLoggedIn = "false"
+console.table([name,age,address,isLoggedIn])
+
+console.log(typeof age) // age=> number
+console.log(typeof null) // null=> object
+console.log(typeof undefined) // undefined => undefined
