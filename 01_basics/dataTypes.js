676bd4f2620b1abae8bc86ad154e6c30eb4a53be
drishti1515 <87563570+drishti1515@users.noreply.github.com>
2023-12-21 16:01:52 +0000

    datatypes

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -6,8 +6,14 @@ let name = " Drishti"
 let age= 21
 let address = "jaipur"
 let isLoggedIn = "false"
-console.table([name,age,address,isLoggedIn])
+// console.table([name,age,address,isLoggedIn])
 
-console.log(typeof age) // age=> number
-console.log(typeof null) // null=> object
-console.log(typeof undefined) // undefined => undefined
+// console.log(typeof age) // age=> number
+// console.log(typeof null) // null=> object
+
+// console.log(typeof undefined) // undefined => undefined
+
+const Id = Symbol('234')
+const anotherId = Symbol ('234')
+
+console.log(Id === anotherId);
