c91452593597228bc55ee1474b10fffd36dd67d3
drishti1515 <87563570+drishti1515@users.noreply.github.com>
2023-12-22 07:18:01 +0000

    stack and heap memory

---
+++ 01_basics/stackAndHeap.js
@@ -0,0 +1,18 @@
+let myName = "drishtiRawat"
+let anotherName= myName
+anotherName = "Guddu"
+console.log(anotherName)
+console.log(anotherName)
+
+
+let userOne ={
+    email: "user@yahoo.com",
+    upi: "user@ybl"
+
+}
+let userTwo = userOne
+
+userTwo.upi = "user2@gmail.com"
+
+console.log(userTwo.upi);
+console.log(userOne.upi)
